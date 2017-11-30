4da39d93d5460c036689f2ad63f2121f81a6c2ed
Ken Shirriff <ken.shirriff@gmail.com>
2017-11-30 14:54:46 -0800

    Add quine2.py, producing a nicer image.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # quine
 Create a quine scarf.
 
+Run "python quine.py" to produce quine.png, an image of quine.py.
+
+Run "python quine2.py" to produce q.png, an image of quine2.py with a nicer background.
+
 Inspired by [fbz's Tweet](https://twitter.com/fbz/status/936117740560990209).
---
+++ quine2.py
@@ -0,0 +1,8 @@
+a="from PIL import Image,ImageDraw;i=Image.new('RGB',(1200,100));d=ImageDraw.Draw(i);"
+b="for n in range(1,1180,13): d.arc((n,n%26,n+37,99-n%26),0,360,fill='white')"
+c="l=260;d.rectangle((l,11,988,90),fill='black');d.text((l,10),'a=%r'%a);d.text((l,20),'b=%r'%b);d.text((l,30),'c=%r'%c);"
+e="d.text((l,40),'e=%r'%e);d.text((l,50),a);d.text((l,60),b);d.text((l,70),c);d.text((l,80),e);i.save('q.png')"
+from PIL import Image,ImageDraw;i=Image.new('RGB',(1200,100));d=ImageDraw.Draw(i);
+for n in range(1,1180,13): d.arc((n,n%26,n+37,99-n%26),0,360,fill='white')
+l=260;d.rectangle((l,11,988,90),fill='black');d.text((l,10),'a=%r'%a);d.text((l,20),'b=%r'%b);d.text((l,30),'c=%r'%c);
+d.text((l,40),'e=%r'%e);d.text((l,50),a);d.text((l,60),b);d.text((l,70),c);d.text((l,80),e);i.save('q.png')
